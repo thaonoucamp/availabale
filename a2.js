a4791c64eff09f3e976797823f36444725d22afd
unknown <dangxuanthaoht@gmail.com>
2020-11-09 11:46:24 +0700

    1

---
+++ a2.js
@@ -0,0 +1,11 @@
+let inputWidth;
+let inputHeight;
+
+inputWidth  = prompt("enter the width");
+inputHeight = prompt("enter the height");
+
+let width   = parseInt(inputWidth);
+let height  = parseInt(inputHeight);
+
+let area    = width * height;
+document.write("the area is: " + area);
---
+++ available1.js
@@ -0,0 +1,9 @@
+let i = 10;
+let f = 20.5;
+let b = true;
+let s = 'ha noi';
+
+document.write('i = ' + i + '<br/>')
+document.write('f = ' + f + '<br/>')
+document.write('b = ' + b + '<br/>')
+document.write('s = ' + s + '<br/>')
